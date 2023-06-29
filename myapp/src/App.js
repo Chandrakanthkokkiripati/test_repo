85e6a4b8bd94b3c3b91c589d53f01c8eaa2bc9a5
Chandrakanth <chandrakanthkokkiripati1998@gmail.com>
2023-06-29 17:00:57 +0530

    files are added

---
+++ myapp/src/App.js
@@ -0,0 +1,10 @@
+import TodosList from './components/TodosList';
+import './App.css';
+
+function App() {
+  return (
+    <TodosList/>
+  );
+}
+
+export default App;
---
+++ myapp/src/components/TodosList/index.js
@@ -0,0 +1,15 @@
+import {Component} from 'react'
+
+import './index.css'
+
+class TodosList extends Component{
+    render(){
+        return(
+            <div className='app-container'>
+                <h1>Todos List</h1>
+            </div>
+        )
+    }
+}
+
+export default TodosList
